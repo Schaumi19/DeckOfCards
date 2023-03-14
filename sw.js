cfa8b0cf2e194c1f090ed833f76eaadbee2a072c
Schaumi19 <manuelschaumberger@gmx.at>
2023-03-14 08:15:38 +0100

    added service worker

---
+++ sw.js
@@ -0,0 +1,27 @@
+
+
+let currentPath = self.location.href.replace("/sw.js", "");
+
+const cacheName = "Deck-of-Cards"
+const assets = [
+    "/",
+    "/index.html",
+    "/deck-of-cards.js"
+   
+].map(url => currentPath + url)
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(cacheName).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+});
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
